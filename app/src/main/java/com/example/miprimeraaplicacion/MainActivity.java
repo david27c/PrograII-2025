2e55f5513fab398440b89be6c66efd1dfc4d365d
david27c <davidcedillos921@gmail.com>
2025-03-11 09:36:43 -0600

    Continuación con las operaciones del CRUD de amigos

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,12 +1,54 @@
 package com.example.miprimeraaplicacion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+    Button btn;
+    TextView tempVal;
+    DB db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DB(this);
+        btn = findViewById(R.id.btnGuardarAmigo);
+        btn.setOnClickListener(view->guardarAmigo());
+
+        fab = findViewById(R.id.fabListaAmigos);
+        fab.setOnClickListener(view->abrirVentana());
+    }
+    private void abrirVentana(){
+        Intent intent = new Intent(this, lista_amigos.class);
+        startActivity(intent);
+    }
+    private void guardarAmigo() {
+        tempVal = findViewById(R.id.txtNombre);
+        String nombre = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtDireccion);
+        String direccion = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtTelefono);
+        String telefono = tempVal.getText().toString();
+        tempVal = findViewById(R.id.txtEmail);
+        String email = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtDui);
+        String dui = tempVal.getText().toString();
+
+        String[] datos = {"", nombre, direccion, telefono, email, dui, ""};
+        db.administrar_amigos("agregar", datos);
+        Toast.makeText(getApplicationContext(), "Registro guardado con exito.", Toast.LENGTH_LONG).show();
+        abrirVentana();
     }
 }
